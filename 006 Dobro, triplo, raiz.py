df444a887301d2c41e1ee733eb8a0a046d962a5c
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-28 15:33:26 -0300

    update

---
+++ 006 Dobro, triplo, raiz.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+d = n*n
+t = n*3
+c = n**2
+print('Dobro= {}  Triplo= {} Cubo= {}'.format(d, t, c))
---
+++ 012 Calculando desconto.py
@@ -0,0 +1,2 @@
+p = float(input('Enter product price: '))
+print('Product price with 5% discount: {:.1f}'.format(p*0.95))
