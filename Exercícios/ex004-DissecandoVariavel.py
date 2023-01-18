4897513e934c192fd38d1d9d50730c0e49f331d8
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-18 07:43:07 -0300

    Create ex004-DissecandoVariavel.py

---
+++ Exercícios/ex004-DissecandoVariavel.py
@@ -0,0 +1,13 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 18/01/23
+# Curso em Vídeo
+
+p = input('Digite algum caracter: ')
+print('Tipo do dado: ', type(p))
+print('Número: ', p.isnumeric())
+print('Alfabeto: ', p.isalpha())
+print('Alfanumérico: ', p.isalnum())
+print('Ascii: ', p.isascii())
+print('Decimal: ', p.isdecimal())
+print('Maiusculo: ', p.isupper())
