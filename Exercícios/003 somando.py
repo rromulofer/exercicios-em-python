803847f2bbd2e80fa263cba0db488ead80366af7
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-18 07:25:55 -0300

    Create 003 somando.py

---
+++ Exercícios/003 somando.py
@@ -0,0 +1,10 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 18/01/23
+# Curso em Vídeo
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+soma = n1+n2
+
+print('A soma entre o número {} e  o número {} é {}'.format(n1, n2, soma))
