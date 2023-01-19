688cdfcbfdc059036c847466c45478bfd843e529
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-19 01:44:46 -0300

    Create ex008-ConversorDeMedidas.py

---
+++ Exercícios/ex008-ConversorDeMedidas.py
@@ -0,0 +1,10 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 18/01/23
+# Curso em Vídeo
+
+me = float(input('Enter a value in meters: '))
+ce = me*100
+mi = me*1000
+print('Value in centimeter: {:.0f}'.format(ce))
+print('Value in millimeter: {:.0f}'.format(mi))
