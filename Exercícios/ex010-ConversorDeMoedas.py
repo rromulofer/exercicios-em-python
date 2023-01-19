3c730a1a489bce7ccea538799eac319cb4b5c8ff
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-19 01:55:47 -0300

    Create ex010-ConversorDeMoedas.py

---
+++ Exercícios/ex010-ConversorDeMoedas.py
@@ -0,0 +1,7 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 19/01/23
+# Curso em Vídeo
+
+m = float(input('Enter the money in R$: '))
+print('You can buy {:.2f} dollars'.format(m/5.19))
