ee42e658064573b04db555090be6dc487739eda6
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-19 01:28:15 -0300

    Create ex007-Media.py

---
+++ Exercícios/ex007-Media.py
@@ -0,0 +1,9 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 18/01/23
+# Curso em Vídeo
+
+n1 = float(input('Digite a nota da p1: '))
+n2 = float(input('Digite a nota da p2: '))
+media = (n1+n2)/2
+print('Média: {}'.format(media))
