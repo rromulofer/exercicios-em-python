b4192493a2c9289bb9b5fd012ba71eda2c858b76
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-20 15:39:12 -0300

    Update ex012-CalculandoDesconto.py

--- Exercícios/ex012-CalculandoDesconto.py
+++ Exercícios/ex012-CalculandoDesconto.py
@@ -2,3 +2,6 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 20/01/23
 # Curso em Vídeo
+
+p = float(input('Enter product price: '))
+print('Product price with 5% discount: {:.1f}'.format(p*0.95))
