fe45901fcbda6011c3407781f15e9fea34348395
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-20 16:28:09 -0300

    Update ex014-ConversorDeTemperatura.py

--- Exercícios/ex014-ConversorDeTemperatura.py
+++ Exercícios/ex014-ConversorDeTemperatura.py
@@ -2,3 +2,7 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 20/01/23
 # Curso em Vídeo
+
+c = float(input('Informe a temperatura em C°: '))
+f = 9 * c / 5 + 32
+print('A temperatura de {}C° corresponde a {}°F'.format(c, f))
