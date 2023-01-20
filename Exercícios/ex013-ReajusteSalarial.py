2bb3d440b4c4c76b79852d10320b5048204e8d09
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-20 16:23:25 -0300

    Update ex013-ReajusteSalarial.py

--- Exercícios/ex013-ReajusteSalarial.py
+++ Exercícios/ex013-ReajusteSalarial.py
@@ -2,3 +2,7 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 20/01/23
 # Curso em Vídeo
+
+import math
+n = float(input('Enter a floating number: '))
+print('Int part: {}'.format(math.floor(n)))
