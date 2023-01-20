bb22d90288677de597c834138bc1f2daf92afce2
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-20 15:38:26 -0300

    Update ex011-PintarParede.py

--- Exercícios/ex011-PintarParede.py
+++ Exercícios/ex011-PintarParede.py
@@ -2,3 +2,7 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 20/01/23
 # Curso em Vídeo
+
+h = float(input('Enter a height: '))
+w = float(input('Enter a width: '))
+print('You can buy {} cans of paint'.format((w*h)/2))
