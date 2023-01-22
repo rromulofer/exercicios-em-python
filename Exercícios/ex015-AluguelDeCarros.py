8d9b20bea2bf4c4f24707fc0140d34dd08166adb
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-21 21:43:02 -0300

    Update ex015-AluguelDeCarros.py

--- Exercícios/ex015-AluguelDeCarros.py
+++ Exercícios/ex015-AluguelDeCarros.py
@@ -2,3 +2,8 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 20/01/23
 # Curso em Vídeo
+
+d = int(input('Dias alugados: '))
+km = float(input('Km rodados:'))
+p = (dias * 60) + (km * 0.15)
+print('O total a pagar é de R${:.2f}'.format(p))
