88eb1c1e5854b6cc085286c5233c8bb85e385d87
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-22 02:58:15 -0300

    Create ex021-TocandoMP3.py

---
+++ Exercícios/ex021-TocandoMP3.py
@@ -0,0 +1,10 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 22/01/23
+# Curso em Vídeo
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
