992da65032933d5f59f9a3a6e3cb4bc15d805720
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-21 21:47:46 -0300

    Update ex017-CatestosHipotenusa.py

--- Exercícios/ex017-CatestosHipotenusa.py
+++ Exercícios/ex017-CatestosHipotenusa.py
@@ -1,4 +1,10 @@
 # Autor: Rômulo Souza Fernandes
 # E-mail: 00119110559@pq.uenf.br
-# Data de criação: 20/01/23
+# Data de criação: 21/01/23
 # Curso em Vídeo
+
+from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
