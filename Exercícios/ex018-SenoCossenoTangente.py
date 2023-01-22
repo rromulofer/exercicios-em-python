3c8c4c76e99cc18883ff4018e761ac36504aa96b
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-21 21:52:42 -0300

    Update ex018-SenoCossenoTangente.py

--- Exercícios/ex018-SenoCossenoTangente.py
+++ Exercícios/ex018-SenoCossenoTangente.py
@@ -1,4 +1,13 @@
 # Autor: Rômulo Souza Fernandes
 # E-mail: 00119110559@pq.uenf.br
-# Data de criação: 20/01/23
+# Data de criação: 21/01/23
 # Curso em Vídeo
+
+from math import radians, sin, cos, tan
+angulo = float(input('Digite o angulo que voce deseja: '))
+seno = sin(radians(angulo))
+print('O angulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
+cosseno = cos(radians(angulo))
+print('O angulo de {} tem o COSSENO de {:.2f}'.format(angulo, cosseno))
+tangente = tan(radians(angulo))
+print('O angulo de {} tem a TANGENTE de {:.2f}'.format(angulo, tangente))
