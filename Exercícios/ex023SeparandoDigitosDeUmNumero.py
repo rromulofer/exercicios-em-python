1dd776e8c2c705e3c605e24bab596a7f50248b35
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-22 02:59:15 -0300

    Create ex023SeparandoDigitosDeUmNumero.py

---
+++ Exercícios/ex023SeparandoDigitosDeUmNumero.py
@@ -0,0 +1,8 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 22/01/23
+# Curso em Vídeo
+
+n = str(input('Type a number: '))
+print('Unity: {}, Dozen: {}, Hundred: {}, Thousand: {}'.format(
+    n[3], n[2], n[1], n[0]))
