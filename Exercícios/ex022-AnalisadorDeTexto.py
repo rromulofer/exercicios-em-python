dce64f1e7035e04eac2411714cdf1f5b95c91dbd
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-22 02:58:55 -0300

    Create ex022-AnalisadorDeTexto.py

---
+++ Exercícios/ex022-AnalisadorDeTexto.py
@@ -0,0 +1,12 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 22/01/23
+# Curso em Vídeo
+
+name = str(input('Type your name: ')).strip()
+print('Uppercase name: {}'.format(name.upper()))
+print('Lowercase name: {}'.format(name.lower()))
+print('Total letters: {}'.format(len(name) - name.count(' ')))
+#print('First name has {} letters. '.format(name.find(' ')))
+s = name.split()
+print('First name has {} letters.'.format(len(s[0])))
