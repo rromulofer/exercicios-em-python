316fb847fa26c6382b2a399fa946cded8c6b76e8
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-21 21:45:21 -0300

    Update ex016-QuebrandoUmNumero.py

--- Exercícios/ex016-QuebrandoUmNumero.py
+++ Exercícios/ex016-QuebrandoUmNumero.py
@@ -1,4 +1,8 @@
 # Autor: Rômulo Souza Fernandes
 # E-mail: 00119110559@pq.uenf.br
-# Data de criação: 20/01/23
+# Data de criação: 21/01/23
 # Curso em Vídeo
+
+import math
+n = float(input('Enter a floating number: '))
+print('Whole parte: {}'.format(math.floor(n)))
