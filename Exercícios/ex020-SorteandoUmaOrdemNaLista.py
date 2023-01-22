ef7d5a58190e15528e3ad4d5c85827cdb37f244c
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-21 22:05:11 -0300

    Update

--- Exercícios/ex020-SorteandoUmaOrdemNaLista.py
+++ Exercícios/ex020-SorteandoUmaOrdemNaLista.py
@@ -1,4 +1,14 @@
 # Autor: Rômulo Souza Fernandes
 # E-mail: 00119110559@pq.uenf.br
-# Data de criação: 20/01/23
+# Data de criação: 21/01/23
 # Curso em Vídeo
+
+from random import shuffle
+n1 = str(input('First student: '))
+n2 = str(input('Second student: '))
+n3 = str(input('Third student: '))
+n4 = str(input('Fourth student: '))
+lista = [n1, n2, n3, n4]
+shuffle(lista)
+print('Order of presentation: ')
+print(lista)
