f4dedeebbe31d55e9619b86ad2549d5127b657e2
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-23 16:49:35 -0300

    Update ex028-JogaDaAdivinhacao.py

--- Exercícios/ex028-JogaDaAdivinhacao.py
+++ Exercícios/ex028-JogaDaAdivinhacao.py
@@ -2,3 +2,13 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 23/01/23
 # Curso em Vídeo
+
+from random import randint
+r = randint(0, 5)
+print('Pensei em um número entre 0 e 5')
+n = int(input('Tente acertar: '))
+
+if r==n:
+    print('Você acertou')
+else:
+    print('Você errou')
