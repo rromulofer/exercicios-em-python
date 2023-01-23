c9c0ffed9fb73d1f7ef346dd6a5225dbf3b146be
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-23 16:42:59 -0300

    Update ex027-PrimeiroeUltimoNomeDeUmaPessoa.py

--- Exercícios/ex027-PrimeiroeUltimoNomeDeUmaPessoa.py
+++ Exercícios/ex027-PrimeiroeUltimoNomeDeUmaPessoa.py
@@ -2,3 +2,8 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 23/01/23
 # Curso em Vídeo
+
+n = str(input('Typer a name: '))
+s = n.split()
+print('First name: {}'.format(s[0]))
+print('Last name: {}'.format(s[len(s)-1]))
