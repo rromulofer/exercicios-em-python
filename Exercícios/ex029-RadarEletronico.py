b0b52b440e0dbb9b48f74c96af263aec56ec102c
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-23 16:54:40 -0300

    Update ex029-RadarEletronico.py

--- Exercícios/ex029-RadarEletronico.py
+++ Exercícios/ex029-RadarEletronico.py
@@ -2,3 +2,11 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 23/01/23
 # Curso em Vídeo
+
+v = float(input('Type a vehicle speed: '))
+if v > 80:
+    t = (v-80)*7
+    print('---You were fined---')
+    print('Fine amount R${}'.format(t))
+else:
+    print('---You were not fined---')
