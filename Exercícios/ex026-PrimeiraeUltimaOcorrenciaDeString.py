c98e57aca8f3c2d9085bed2ea57973534a94c773
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-23 16:39:27 -0300

    Update ex026-PrimeiraeUltimaOcorrenciaDeString.py

--- Exercícios/ex026-PrimeiraeUltimaOcorrenciaDeString.py
+++ Exercícios/ex026-PrimeiraeUltimaOcorrenciaDeString.py
@@ -2,3 +2,8 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 23/01/23
 # Curso em Vídeo
+
+n = str(input('Type a name: ')).upper()
+print('Amount of A: {}'.format(n.count('A')))
+print('First A: {}'.format(n.find('A')+1))
+print('Last A: {}'.format(n.rfind('A')+1))
