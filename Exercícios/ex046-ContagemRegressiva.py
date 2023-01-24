330c9faad4afd81cff79c1c1bcc055b799ad1744
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-24 01:28:06 -0300

    Create ex046-ContagemRegressiva.py

---
+++ Exercícios/ex046-ContagemRegressiva.py
@@ -0,0 +1,12 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 24/01/23
+# Curso em Vídeo
+
+from time import sleep
+
+for cont in range(10, -1, -1):
+    print(cont)
+    sleep(0.5)
+
+print("FIM!")
