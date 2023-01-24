83188b06aa1bf087a71a62567b3d144aa92f4881
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-24 02:06:18 -0300

    Update ex051-ProgressaoAritmetica.py

--- Exercícios/ex051-ProgressaoAritmetica.py
+++ Exercícios/ex051-ProgressaoAritmetica.py
@@ -3,3 +3,8 @@
 # Data de criação: 24/01/23
 # Curso em Vídeo
 
+p = int(input('Digite o primeiro termo: '))
+r = int(input('Digite a razão: '))
+d = (p + (10-1) * r)
+for x in range(p, d+r, r):
+    print('{}'.format(x), end=' > ')
