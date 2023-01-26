157fd451845feccc75e36e6623a9153a7650d8e7
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-26 02:31:53 -0300

    Update ex034-AumentosMultiplos.py

--- Exercícios/ex034-AumentosMultiplos.py
+++ Exercícios/ex034-AumentosMultiplos.py
@@ -2,3 +2,10 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 23/01/23
 # Curso em Vídeo
+
+s = float(input('Type the salary: '))
+if s>1250:
+    n = s+(s*10/100)
+else:
+    n = s+(s*15/100)
+print('Salary increased to R${:.2f}'.format(n))
