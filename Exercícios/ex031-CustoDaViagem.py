c7607857de362cb176c61b34e235f28f1428a1e8
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-26 02:28:10 -0300

    Update ex031-CustoDaViagem.py

--- Exercícios/ex031-CustoDaViagem.py
+++ Exercícios/ex031-CustoDaViagem.py
@@ -2,3 +2,9 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 23/01/23
 # Curso em Vídeo
+
+km = float(input('Enter the trip distance in kilometers: '))
+if km<=200:
+    print('Value: R${}'.format(km*0.50))
+else:
+    print('Value: R${}'.format(km*0.45))
