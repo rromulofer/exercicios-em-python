858b4fec1334197c45b5bd210bbccd6dc78bcadf
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-26 02:29:05 -0300

    Update ex032-AnoBissexto.py

--- Exercícios/ex032-AnoBissexto.py
+++ Exercícios/ex032-AnoBissexto.py
@@ -2,3 +2,9 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 23/01/23
 # Curso em Vídeo
+
+y = int(input('Type a year: '))
+if y%4==0 and y%100!=0 or y%400==0:
+    print('{} is a leap year'.format(y))
+else:
+    print('{} isnt a leap year'.format(y))
