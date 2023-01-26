814c1415d17b7124155ed04a03d8054cbd7cc3c8
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-01-26 02:31:04 -0300

    Update ex033-MaioreMenor.py

--- Exercícios/ex033-MaioreMenor.py
+++ Exercícios/ex033-MaioreMenor.py
@@ -2,3 +2,20 @@
 # E-mail: 00119110559@pq.uenf.br
 # Data de criação: 23/01/23
 # Curso em Vídeo
+
+n1 = int(input('Type a number: '))
+n2 = int(input('Type another number: '))
+n3 = int(input('Type another number: '))
+ma = n1
+if n2>n1 and n2>n3:
+    ma = n2
+if n3>n1 and n3>n2:
+    ma = n3
+
+me = n1
+if n2<n1 and n2<n3:
+    me = n2
+if n3<n1 and n3<n2:
+    me = n3
+
+print('{} is a bigger number and {} is a smaller number'.format(ma,me))
