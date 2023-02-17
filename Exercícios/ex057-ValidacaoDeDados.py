c15e79a685a2b549eb287361b4348f835ca63886
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-02-17 02:09:56 -0300

    Update ex057-ValidacaoDeDados.py

--- Exercícios/ex057-ValidacaoDeDados.py
+++ Exercícios/ex057-ValidacaoDeDados.py
@@ -1,3 +1,8 @@
+# Autor: Rômulo Souza Fernandes
+# E-mail: 00119110559@pq.uenf.br
+# Data de criação: 17/02/23
+# Curso em Vídeo
+
 sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
 while sexo not in 'MF':
     sexo = str(input('Dados inválidos. Informe novamente: ')).strip().upper()[0]
