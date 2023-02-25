f10a553807f825a7580140e34c47a47633919b8e
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-02-25 01:23:14 -0300

    Update

--- Exercícios/ex083B-ValidandoExpressoesMatematicas.py
+++ Exercícios/ex083B-ValidandoExpressoesMatematicas.py
@@ -3,6 +3,10 @@
 # Data de criação: 23/02/23
 # Curso em Vídeo
 
+# Exercício Python 083: Crie um programa onde o usuário digite uma expressão 
+# qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão 
+# passada está com os parênteses abertos e fechados na ordem correta.
+
 pilha = list()
 abre = 0
 fecha = 0 
